d0164cbc388966cf7a2e8c8ec94674320628ff6a
Jannis R <mail@jannisr.de>
2016-06-18 03:55:20 +0200

    implementation :clap:

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 'use strict'
 
-// todo
+const got = require('got')
+const cfg = require('./package.json').config
+
+setInterval(() => {
+	got(cfg.portal, {json: true}).then((res) => {
+
+		const data = Object.assign({when: Math.round(Date.now() / 1000)}, res.body)
+		got.post(cfg.cloud, {body: data}).catch(console.error)
+
+	}, console.error)
+}, cfg.interval * 1000)
