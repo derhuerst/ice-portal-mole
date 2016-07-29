d5d370156b20680a17f276c886453c37cbca68fb
Jannis R <mail@jannisr.de>
2016-07-29 23:58:06 +0200

    use actual ICE portal

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ const got = require('got')
 const cfg = require('./package.json').config
 
 setInterval(() => {
-	got(cfg.portal, {json: true}).then((res) => {
-
-		const data = Object.assign({when: Math.round(Date.now() / 1000)}, res.body)
-		got.post(cfg.cloud, {body: data}).catch(console.error)
+	Promise.all(
+		got(cfg.portal + '/api1/rs/status', {json: true})
+		got(cfg.portal + '/api1/rs/tripInfo', {json: true})
+	).then([status, trip] => {
+		got.post(cfg.cloud, {body: {
+			  when: Math.round(Date.now() / 1000)
+			  connected: status.body.connection
+			, speed: status.body.speed
+			, latitude: status.body.latitude
+			, longitude: status.body.longitude
+			, train: [trip.body.trainType, trip.body.vzn]
+			, traveledDistance: trip.body.actualPosition
+			, totalDistance: trip.body.totalDistance
+		}}).catch(console.error)
 
 	}, console.error)
 }, cfg.interval * 1000)
